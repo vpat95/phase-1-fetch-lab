eb0b03855404458fafe34fd5ce313d9da1725a21
Vivek Patel <vpatel360@gmail.com>
2023-07-14 16:45:57 -0400

    commit tree d7afa55e546c625ebc9b87e9849c9c97e7545da6

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(books){
+  return books
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
